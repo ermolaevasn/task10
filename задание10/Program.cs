e99ac5e09fa3d6f333075cfc112f2da14d815b5d
Дом <@DESKTOP-7GF1H2I>
2017-08-16 16:16:58 +0500

    +если не упорядочена

--- задание10/Program.cs
+++ задание10/Program.cs
@@ -10,7 +10,7 @@ namespace задание10
     {
         static void Main(string[] args)
         {
-            int n,min,k=0,m=0;
+            int n,m,k;
             Console.WriteLine("Введите количество чисел n");
             n = Convert.ToInt32(Console.ReadLine());
             int[] mas = new int[n];
@@ -19,9 +19,26 @@ namespace задание10
                 Console.Write("Введите число "+(i+1)+": ");
                 mas[i] = Convert.ToInt32(Console.ReadLine());
             }
+
             for (int i = 0; i < n; i++)
                 Console.Write(mas[i]+"  ");
-            
+
+            k = n - 1;//если не упорядочен
+            m = n - 1;
+            for (int j = 0; j < n; j++)
+            {
+                m = mas[0];
+                for (int i = 0; i < k; i++)
+                {
+                    mas[i] = mas[i + 1];  
+                }
+                mas[k] = m;
+                  k--;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+                Console.Write(mas[i] + "  ");
+
             Console.ReadKey();
 
         }
